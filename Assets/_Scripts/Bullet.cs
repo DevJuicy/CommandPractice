ce4fa500fb998bdc9123af9c5b08f37b2e1c5550
DevJuicy <bassoff@naver.com>
2020-04-15 14:31:58 +0900

    feat : 총알 발사 로직 추가

---
+++ Assets/_Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 5f;
+
+     void Update()
+    {
+        transform.position += transform.up * moveSpeed * Time.deltaTime;
+    }
+
+    public void Activate(Vector3 startPosition, Vector3 targetPosition)
+    {
+        transform.position = startPosition;
+        Vector3 dir = (targetPosition - startPosition).normalized;
+        transform.rotation = Quaternion.LookRotation(transform.forward, dir);
+    }
+}
--- Assets/_Scripts/BulletLauncher.cs
+++ Assets/_Scripts/BulletLauncher.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class BulletLauncher : MonoBehaviour
 {
+    [SerializeField]
+    Bullet bulletPrefab;
+    Bullet bullet;
+
+    [SerializeField]
+    Transform firePosition;
     public void OnFireButtonPressed(Vector3 position)
     {
         Debug.Log("Fired a bullet" + position);
+        bullet = Instantiate(bulletPrefab);
+        bullet.Activate(firePosition.position, position);
     }
 }
