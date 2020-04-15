a616b432ca6ca042c899f0f1bf3bd406c3a5c84e
DevJuicy <bassoff@naver.com>
2020-04-15 15:54:22 +0900

    feat : Object pooling 방식의 총알로직

--- Assets/_Scripts/Bullet.cs
+++ Assets/_Scripts/Bullet.cs
@@ -1,21 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Bullet : MonoBehaviour
+public class Bullet : RecycleObject
 {
     [SerializeField]
     float moveSpeed = 5f;
 
-     void Update()
+    Vector3 targetPosition;
+    bool isActivated;
+
+    public Action<Bullet> Destroyed;
+
+    void Update()
     {
+        if (!isActivated)
+            return;
+
         transform.position += transform.up * moveSpeed * Time.deltaTime;
+
+        if (IsArrivedToTarget())
+        {
+            isActivated = false;
+            Destroyed?.Invoke(this);
+        }
     }
 
     public void Activate(Vector3 startPosition, Vector3 targetPosition)
     {
         transform.position = startPosition;
+        this.targetPosition = targetPosition;
         Vector3 dir = (targetPosition - startPosition).normalized;
         transform.rotation = Quaternion.LookRotation(transform.forward, dir);
+        isActivated = true;
+    }
+
+    bool IsArrivedToTarget()
+    {
+        float distance = Vector3.Distance(transform.position, targetPosition);
+        return distance < 0.1f;
     }
 }
--- Assets/_Scripts/BulletLauncher.cs
+++ Assets/_Scripts/BulletLauncher.cs
@@ -6,14 +6,28 @@ public class BulletLauncher : MonoBehaviour
 {
     [SerializeField]
     Bullet bulletPrefab;
-    Bullet bullet;
 
     [SerializeField]
     Transform firePosition;
+
+    Factory bulletFactory;
+
+    void Start()
+    {
+        bulletFactory = new Factory(bulletPrefab);
+    }
+
     public void OnFireButtonPressed(Vector3 position)
     {
         Debug.Log("Fired a bullet" + position);
-        bullet = Instantiate(bulletPrefab);
+        Bullet bullet = bulletFactory.Get() as Bullet;
         bullet.Activate(firePosition.position, position);
+        bullet.Destroyed += OnBulletDestroyed;
+    }
+
+    void OnBulletDestroyed(Bullet usedBullet)
+    {
+        usedBullet.Destroyed -= OnBulletDestroyed;
+        bulletFactory.Restore(usedBullet);
     }
 }
