a9e35ae80b3dd314e31091b7cdbac111d7fbc528
DevJuicy <bassoff@naver.com>
2020-04-16 14:20:48 +0900

    feat : 발사체 위치 조정

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -7,9 +7,13 @@ public class GameManager : MonoBehaviour
     BulletLauncher launcherPrefab;
     BulletLauncher launcher;
 
+    [SerializeField]
+    Transform launcherLocator;
+
     void Start()
     {
         launcher = Instantiate(launcherPrefab);
+        launcher.transform.position = launcherLocator.position;
 
         MouseGameController mouseGameController = gameObject.AddComponent<MouseGameController>();
         // MouseGameController는 MonoBehaviour 를 상속 받기 때문에 더이상 new 는 불가능
