03bc89ce991356064cba2f8c05614ce1cb38e095
DevJuicy <bassoff@naver.com>
2020-04-16 17:19:36 +0900

    feat : missile 충돌 시 폭발이펙트 생성

---
+++ Assets/_Scripts/DestroyEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEffect : RecycleObject
+{
+    [SerializeField]
+    float timeToRemove = 1f;
+    float elapsedTime = 0f;
+
+    void Update()
+    {
+        if (isActivated)
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime >= timeToRemove)
+            {
+                elapsedTime = 0f;
+                DestroySelf();
+            }
+        }
+    }
+
+    void DestroySelf()
+    {
+        isActivated = false;
+        Destroyed?.Invoke(this);
+    }
+}
